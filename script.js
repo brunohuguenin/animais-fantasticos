b4342b1c99acaecf6fa3f18a47b477e25c6626d2
Brunohugueninn <brunohugueninn@gmail.com>
2022-12-04 10:27:46 -0300

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const animais = document.getElementById('animais')
+console.log(animais.innerText)
+
+const gridSection = document.getElementsByClassName('grid-section')
+
+console.log(gridSection.leght)
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+const animaisImg = document.querySelectorAll('.animais img')
+console.log(animaisImg)
