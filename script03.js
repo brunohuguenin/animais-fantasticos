15999708a1d05ec6351d6d44eab66307417fbd97
Brunohugueninn <brunohugueninn@gmail.com>
2022-12-05 17:45:13 -0300

    commit

---
+++ script03.js
@@ -0,0 +1,9 @@
+const listaAnimais = document.querySelector('.animais-lista');
+const height = listaAnimais.scrollHeight
+const animaisTop = listaAnimais.offsetTop
+console.log(animaisTop)
+
+
+console.log(
+    
+)
