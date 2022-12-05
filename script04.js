5dc73e653f6e9eb830c11f5d80b2310040aca2af
Brunohugueninn <brunohugueninn@gmail.com>
2022-12-05 19:57:51 -0300

    commit

--- script04.js
+++ script04.js
@@ -24,17 +24,26 @@ imags.forEach((img) =>{
 const linksInternos = document.querySelectorAll('a[href^="#"]');
 
 function handleLink(event){
+    event.preventDefault();
+    linksInternos.forEach((link) => {
+        link.classList.remove('ativo');
+    });
+    event.currentTarget.classList.add('ativo');
     console.log(event)
 }
 
 linksInternos.forEach((link) => {
-    link.addEventListener('click', handleLink)
+    link.addEventListener('click', handleLink);
 })
 
 
 
-// Selecione todos os elementos do site começando a partir do body,
-// ao clique mostre exatamente quais elementos estão sendo clicados
+// Selecione todos os elementos do site começando a partir do body, ao clique mostre exatamente quais elementos estão sendo clicados
+
+const todosElements = document.querySelectorAll('body *');
+console.log(todosElements)
+
+
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 // Se o usuário clicar na tecla (t), aumente todo o texto do
