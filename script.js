ea01c566c58186f3a105744055f93916050a83bd
Brunohugueninn <brunohugueninn@gmail.com>
2022-12-09 20:34:38 -0300

    commit

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function initTabNav() {
         });
     }
 }
-
 initTabNav();
 
 
@@ -45,5 +44,49 @@ initAccordion();
 
 
 // Scrooll-suave para links internos
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        const topo = section.offsetTop
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+
+        // forma alternativa
+    /*window.scrollTo ({
+            top: topo,
+            behavior: 'smooth'
+        })*/
+    }
+    linksInternos.forEach((link) =>{
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
+
+
+// Animação-ao-scrool
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+initAnimacaoScroll();
