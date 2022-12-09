cded1b7dc645e4bdbd08a52ecee26e1e0f673626
Brunohugueninn <brunohugueninn@gmail.com>
2022-12-09 18:23:14 -0300

    commit

--- script.js
+++ script.js
@@ -1,83 +1,20 @@
-const animais = document.getElementById('animais')
-console.log(animais.innerText)
+// Navegação por TAB
 
-const gridSection = document.getElementsByClassName('grid-section')
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabcontent = document.querySelectorAll('.js-tabcontent section');
+tabcontent[0].classList.add('ativo')
 
-console.log(gridSection.leght)
+if (tabmenu.length && tabcontent.length) {
+    function activtab(index) {
+        tabcontent.forEach((section) => {
+            section.classList.remove('ativo');
+        })
+        tabcontent[index].classList.add('ativo');
+    }
 
-const primeiraLi = document.querySelector('li')
-console.log(primeiraLi)
-
-const primeiraUl = document.querySelector('ul')
-console.log(primeiraUl)
-
-const animaisImg = document.querySelectorAll('.animais img')
-console.log(animaisImg[2])
-
-const gridSectionHTML = document.getElementsByClassName('grid-section')
-const gridSectionNode = document.querySelectorAll('.grid-section')
-
-console.log(gridSectionHTML)
-console.log(gridSectionNode)
-
-
-/*
-// Retorne no sonsole todas as imagens do site
-
-const img = document.querySelectorAll('img');
-console.log(img)
-
-// Retorne no sonsole apenas as imagens que começam com a palavra 'imagem'
-
-const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]')
-console.log(imagensAnimais)
-
-// Selecione os links internos (onde o href começa com '#')
-
-const linksInterno = document.querySelectorAll('[href^="#')
-console.log(linksInterno)
-
-// Selecione a primeira h2 dentro de .animais-descricao
-
-const subTitulo = document.querySelector('.animais-descricao h2')
-console.log(subTitulo)
-
-// Selecione o último p do site
-
-const paragrafos = document.querySelectorAll('p')
-console.log(paragrafos[paragrafos.length - 1])*/
-
-/*const imgs = document.querySelectorAll('img');
-
-let i = 0;
-imgs.forEach(function(item, index, array){ //pode ser qualquer nome pois é uma var
-    console.log(item, index, array)
-})*/
-
-/*const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos);
-
-titulosArray.forEach(function(item) {
-    console.log(item)
-});*/
-
-/*const imgs = document.querySelectorAll('img');
-
-imgs.forEach((item, index) => {
-    console.log(item);
-});*/
-
-// 1º
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos)
-
-paragrafos.forEach((item) => {
-    console.log(item)
-})
-
-// 2º
-paragrafos.forEach((item) => {
-    console.log(item.innerText)
-})
-
-// 3º
+    tabmenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', function(){
+            activtab(index);
+        })
+    });
+}
